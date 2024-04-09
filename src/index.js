da30e88f0a1e3da6b4b066ac1ec5fbd0ed4f9e6a
Ghayoor Hussain <ghayoorhussain701@gmail.com>
2024-04-10 02:24:08 +0500

    Worked on User and Videos models with jwt and bcrypt

--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
 import "dotenv/config";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.on("error", (error) => {
+      "ERROR in DB Connection";
+      throw error;
+    });
+    app.listen(process.env.PORT || 5000, () => {
+      console.log(`Server is running at port : ${process.env.PORT}`);
+    });
+  })
+  .catch((error) => console.log("MONGODB Connection Failed ", error.message));
 /*
 import express from "express";
 const app = express();
