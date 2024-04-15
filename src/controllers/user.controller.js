6fcc1a5691e968fd9b98d661d929e397fe065fbf
Ghayoor Hussain <ghayoorhussain701@gmail.com>
2024-04-16 02:23:10 +0500

    Register User bug fixes

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -23,20 +23,26 @@ const registerUser = asyncHandler(async (req, res) => {
     throw new ApiError(400, "All Fields are Required");
   }
 
-  const existedUser = User.findOne({
+  const existedUser = await User.findOne({
     $or: [{ userName }, { email }],
   });
 
   if (existedUser)
     throw new ApiError(409, `User with email or username is already exist`);
-
   const avatarLocalPath = req.files?.avatar[0]?.path;
-  const coverImageLocalPath = req.files?.coverImage[0].path;
+  // const coverImageLocalPath = req.files?.coverImage[0].path;
 
+  let coverImageLocalPath;
+  if (
+    req.files &&
+    Array.isArray(req.files.coverImage) &&
+    req.files.coverImage.length > 0
+  ) {
+    coverImageLocalPath = req.files.coverImage[0].path;
+  }
   if (!avatarLocalPath) throw new ApiError(400, "Avatar file is require");
 
   // upload to cloudinary
-
   const avatar = await uploadOnCloudinary(avatarLocalPath);
   const coverImage = await uploadOnCloudinary(coverImageLocalPath);
 
